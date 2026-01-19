7677c998192fe376e27338ce280481b066b94aa2
Ritzss <ritanshubabuta399@gmail.com>
2026-01-19 14:47:00 +0530

    first deploy version 0.1

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,4 +1,3 @@
-import { notFound } from "next/navigation";
 import ProductClient from "./ProductClient";
 
 type PageProps = {
@@ -26,18 +25,12 @@ const normalizedCategory = category
   .toLowerCase()
   .replace(/\/$/, "");
 
-if (!allowed.includes(normalizedCategory)) {
-  notFound();
-}
+
 
 
   const res = await fetch("https://fakestoreapi.com/products", {
     cache: "no-store",
   });
-
-  if (!res.ok) {
-    notFound();
-  }
 
   const products = await res.json();
 
