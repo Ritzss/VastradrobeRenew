bfbaeeacc532c7dac9a9dcef718693d7388ae45e
Ritzss <ritanshubabuta399@gmail.com>
2026-02-19 13:40:31 +0530

    version2.4.3 -updated reset logic

--- app/api/auth/send-otp/route.ts
+++ app/api/auth/send-otp/route.ts
@@ -9,19 +9,31 @@ export async function POST(req: Request) {
     const { email } = await req.json();
 
     if (!email) {
+      // console.log("❌ Email missing");
       return NextResponse.json({ error: "Email required" }, { status: 400 });
     }
+
+    // console.log("📩 OTP request for:", email);
 
     await connectDB();
 
     const user = await User.findOne({ email });
 
-    // Prevent email enumeration
-    if (!user) {
+    if(user){
       return NextResponse.json({ success: true });
     }
 
+    if (!user) {
+      // console.log("⚠️ User not found:", email);
+      // still returning success to prevent enumeration
+      return NextResponse.json({ success: false });
+    }
+
+    // console.log("✅ User found:", user.email);
+
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    // console.log("🔐 Generated OTP:", otp);
+
     const hashedOtp = await bcrypt.hash(otp, 10);
 
     user.resetOtp = hashedOtp;
@@ -30,13 +42,23 @@ export async function POST(req: Request) {
     user.resetVerified = false;
 
     await user.save();
+    // console.log("💾 OTP saved to DB");
 
-    await sendOtpEmail(email, otp);
+    try {
+      await sendOtpEmail(email, otp);
+      // console.log("📧 OTP email sent successfully");
+    } catch (emailError) {
+      // console.error("❌ Email send failed:", emailError);
+      return NextResponse.json(
+        { error: "Failed to send OTP email" },
+        { status: 500 }
+      );
+    }
 
     return NextResponse.json({ success: true });
 
   } catch (err) {
-    console.error(err);
+    // console.error("🔥 Server error:", err);
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
