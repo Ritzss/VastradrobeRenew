0cfe542c093c11e54528f8640ce8bfa7664db559
Ritzss <ritanshubabuta399@gmail.com>
2026-02-19 11:38:28 +0530

    version2.4.3 -added active for mobile view

--- app/components/Home/ScrollRevealProducts.tsx
+++ app/components/Home/ScrollRevealProducts.tsx
@@ -81,13 +81,13 @@ const filteredProducts = products.filter((product) =>
             <div key={product.productId} className="w-full sm:w-[60%] shrink-0">
               <ProductCard
                 product={product}
-                className="md:w-[95%] group w-full hover:scale-102 hover:shadow-[0_0_10px] duration-700 transition-all"
-                height="h-[55vh]"
+                className="md:w-[95%] group w-full active:scale-102 active:shadow-[0_0_10px_#000] duration-700 transition-all"
+                height="h-[60vh]"
                 classNameInner="h-[47vh] rounded-2xl"
                 button={false}
               >
               <div className="uppercase text-center m-2 w-full ">
-                {product.name} <span className="hidden group-hover:block">{` (${product.color})`}</span>
+                {product.name} <span className="hidden group-active:block">{` (${product.color})`}</span>
               </div>
             </ProductCard>
             </div>
