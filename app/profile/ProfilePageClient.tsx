e45e2c7401f6c62df32654bd3a383e9adcf5aaa8
Ritzss <ritanshubabuta399@gmail.com>
2026-02-24 16:44:28 +0530

    version2.5.6-24/2/26

--- app/profile/ProfilePageClient.tsx
+++ app/profile/ProfilePageClient.tsx
@@ -247,7 +247,7 @@ const ProfilePageClient = () => {
                 My Favorites
               </div>
             </Link>
-            <Link href={"reset_password"}>
+            <Link href={"account/reset_password"}>
               <div className="profileBox text-center">
                 Password Reset
               </div>
