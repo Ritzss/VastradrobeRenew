8312c2d653101e9d734f9e92af3daef0bb5d8a4e
Ritzss <ritanshubabuta399@gmail.com>
2026-06-12 11:04:35 +0530

    minor changes in ui

--- app/components/Home/LandingSlider.tsx
+++ app/components/Home/LandingSlider.tsx
@@ -8,7 +8,7 @@ import { Autoplay, Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
-import { landingPages } from "@/Data/LandingPges";
+import { landingPages } from "@/Data/LandingPages";
 
 // import { landingPages } from "@/data/landingPages";
 
