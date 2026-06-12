0ac90eccb92f8a7e144c7ac3b6ccea93503f4f5e
Ritzss <ritanshubabuta399@gmail.com>
2026-06-12 11:58:54 +0530

    minor changes in ui

--- app/Data/LandingPages.ts
+++ app/Data/LandingPages.ts
@@ -32,3 +32,163 @@ export const landingPages = [
   //   featured: true,
   // },
 ];
+
+
+export const productsData = [
+        {
+          productId: 1,
+          c: "olive",
+          hex: "#6B7A5A",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118560/products/p1samry4giprhoi6cemg.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118561/products/dnvdr1kmhhhdhbwdyr3d.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118561/products/as6dgs2imoihxqu5qm6u.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118562/products/yvwgxexxgilvhk1sflqg.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118563/products/hh740rpq84z5v51xql2j.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "pink",
+          hex: "#D47D8A",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118432/products/q18e4kxmojkxtnhlfbmr.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118432/products/ldfmn4ntq7ajq7mrhnof.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118433/products/avuovoeo37ccsp384zoa.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118433/products/lstuwnrks2zimkh0jocm.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118434/products/i3qwy0mszmvrbzceniz8.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "rust brown",
+          hex: "#B5653A",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118473/products/hdo05pzatlvfoj3kt05r.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118474/products/fhikcoceuvyxlf5ynbql.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118474/products/rin0peytomcopicrb4hr.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118475/products/oisctbd14htpb0fei2ao.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118476/products/utzxini6rshvk7na0bsl.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "red brown",
+          hex: "#9B4A4A",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780047280/products/nlj0y1zvnafsplhflvpd.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780047281/products/w3pv2ozf2s2mpp81mwr8.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780047281/products/cti5mmgae0b75esz33uu.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780047282/products/y4dambb8xa1ngrpct7u5.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780047282/products/snybparfn9cmfdltsuqw.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "rose pink",
+          hex: "#E8A0A8",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117592/products/uq8hvsqirwmgsq34cplj.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117593/products/lbn9xbcralnlpcsap4v4.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117594/products/hv7hpyikqwp2rymsroja.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117594/products/linkcl7cyonwxecvioto.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117595/products/f0sxuyfifwaapqqkc3db.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "blue",
+          hex: "#3D5A80",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117709/products/vc5obt2dxkizvrclouky.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117709/products/rxzj6uf6z9bv7sagtb0f.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117710/products/rhnnpaqmx73uy2db3kqa.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117710/products/z4e4fshcuucr9euebxlo.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780117711/products/ol9wlnjac7mpqywxf8fp.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "baby pink",
+          hex: "#F0B8C0",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118004/products/h2ppim0n1lndtznpzurp.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118005/products/gmx1jizun7juetvugbuy.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118005/products/f2vxofxfrf4v3tsoxaji.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118006/products/p6uyehghzohkjzozap9h.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118006/products/wyb5wwfljt7bfsg5avij.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "beige",
+          hex: "#D4C4B0",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118174/products/pp95mvjgf3hamtz3zmtk.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118174/products/it3vn1fm62c6x7vcyx7k.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118173/products/shi7x8os0dlliu48nby9.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118173/products/rh7xvjzaemyw9i3njxhx.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780118172/products/ou3qgr5s8ifgf7tg26or.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "green",
+          hex: "#4A7C59",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1779187903/products/dlqcmbd75htigkwpay6b.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1779187904/products/nsuzubfa4u8x1unlokeu.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1779187905/products/dxmfw0figs4v5pvwvfvl.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1779187907/products/jlaf0pmj6tkesvx9cewu.png",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1779187908/products/rbhifnnxklg36c5cvyak.png",
+          ],
+        },
+        {
+          productId: 1,
+          c: "coffee brown",
+          hex: "#7B5E4A",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780572215/products/egu8q088skkm50tw3aja.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780572216/products/jvntergoaa1m1ecp6loc.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780572216/products/tlyewnzuiwg31tsrvacy.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780572217/products/lxyk4nwmirde0t5xzpdl.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780572217/products/xx2f92vsv4woota0n18o.jpg",
+          ],
+        },
+        {
+          productId: 1,
+          c: "black",
+          hex: "#2C2825",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780571755/products/nxsq3bk0mrbxzmny1gv3.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780571755/products/nygg86itibbjk0a2rtsi.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780571756/products/zyf3kk9ctprsg4sv43iq.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780571757/products/mo9fd7jdjw9tacmfmhwz.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1780571757/products/shokyrkttiu0vgvzarov.jpg",
+          ],
+        },
+        {
+          productId: 1,
+          c: "wine",
+          hex: "#7D3B3B",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773383830/products/kxrllzolwzko9xw5ma3s.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773383824/products/vwop2qz6ugyiafqp2wxs.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773383825/products/wmvfxd6gr7jqk0khxjcs.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773383827/products/c1sbegzoaxws6ghtpmb9.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773383828/products/qm8voyzw1jkdvbxadatl.jpg",
+          ],
+        },
+        {
+          productId: 1,
+          c: "light brown",
+          hex: "#C4A882",
+          imgs: [
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773390341/products/xht3kwkhqiu7ix2ufa3l.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773390340/products/guwshdxgiyhxmvjtqd9w.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773390338/products/wo83u4htkvmgtrxsv9cf.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773390337/products/xm6xuiyu0gut5bg0m6mg.jpg",
+            "https://res.cloudinary.com/dwhn5ec09/image/upload/v1773390336/products/cnbsxhyohwqpsja1cwjp.jpg",
+          ],
+        },
+      ];
