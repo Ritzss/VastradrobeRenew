40e77270d47636ed2aae06c72ecb00d80d18350b
Ritzss <ritanshubabuta399@gmail.com>
2026-06-22 13:16:55 +0530

    updated ui

--- app/components/Global/ProductCardStatic.tsx
+++ app/components/Global/ProductCardStatic.tsx
@@ -40,7 +40,7 @@ export default function ProductCardStatic({
       </Link>
 
       <div className="mt-4 text-center">
-        <p className="text-[14px] font-medium text-[#5f5143]">{product.name}</p>
+        <p className="text-[14px] font-medium text-[#5f5143] line-clamp-2">{product.name}</p>
 
         {product.price && (
           <p className="mt-1 text-[13px] text-[#957f6a]">₹{product.price}</p>
