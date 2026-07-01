8da5b4d1e749957436dbd3318c813e40b7b4bb25
Ritzss <ritanshubabuta399@gmail.com>
2026-07-01 15:09:33 +0530

    updated ui

--- app/components/products/ClientCatLayout.tsx
+++ app/components/products/ClientCatLayout.tsx
@@ -33,7 +33,7 @@ const ClientCatLayout = ({ children }: { children: ReactNode }) => {
           <SideFilter />
         </aside>
 
-        <main className="flex-1 scrollbar-hide overflow-y-scroll h-[116vh]">{children}</main>
+        <main id="categoryPage" className="flex-1 scrollbar-hide overflow-y-scroll h-[116vh]">{children}</main>
       </div>
     </section>
   );
